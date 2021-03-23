47dc26a42a6ddb84a05665bac223b45636f1cd10
Alexander Barth <barth.alexander@gmail.com>
2021-03-23 13:35:13 +0100

    update roms-3.9 compile script

--- test/compile_run_ROMS.sh
+++ test/compile_run_ROMS.sh
@@ -1,5 +1,3 @@
-# Get
-# build_roms.sh.patch
 # ROMS source code in ~/src/roms
 # ~/Data/Bathymetry/combined_emodnet_bathymetry.nc
 # ~/Data/Atmosphere/ecmwf_operational_archive_2018-12-01T00:00:00_2020-01-01T00:00:00.nc
@@ -10,8 +8,7 @@ BUILD_DIR="$HOME/ROMS-implementation-test"
 mkdir -p ~/src/
 cd ~/src/
 svn checkout --no-auth-cache --non-interactive  --username "$ROMS_USERNAME" \
-    --password "$ROMS_PASSWORD" https://www.myroms.org/svn/src/tags/roms-3.9
-
+    --password "$ROMS_PASSWORD" https://www.myroms.org/svn/src/tags/roms-3.9 roms
 cd ~/src/roms
 echo svnversion $(svnversion)
 
@@ -19,13 +16,34 @@ mkdir -p "$BUILD_DIR"
 cd "$BUILD_DIR"
 
 wget -O liguriansea.h 'https://dox.ulg.ac.be/index.php/s/9yqxI4tp5hNr2Sg/download'
-wget -O build_roms.sh.patch 'https://dox.ulg.ac.be/index.php/s/9cha2VbR3DYvZHd/download'
 
 # get compile script
 cp ~/src/roms/ROMS/Bin/build_roms.sh build_roms.sh
 
-# modify compile script
-patch < build_roms.sh.patch
+patch build_roms.sh <<EOF
+105c105
+< export   ROMS_APPLICATION=UPWELLING
+---
+> export   ROMS_APPLICATION=LigurianSea
+110c110
+< export        MY_ROOT_DIR=\${HOME}/ocean/repository
+---
+> export        MY_ROOT_DIR=\${HOME}
+123c123
+<  export       MY_ROMS_SRC=\${MY_ROOT_DIR}/trunk
+---
+>  export       MY_ROMS_SRC=\${MY_ROOT_DIR}/src/roms
+171,172c171,172
+<  export              FORT=ifort
+< #export              FORT=gfortran
+---
+> #export              FORT=ifort
+> export              FORT=gfortran
+177c177
+< #export       USE_NETCDF4=on            # compile with NetCDF-4 library
+---
+>  export       USE_NETCDF4=on            # compile with NetCDF-4 library
+EOF
 
 if [ -e build_roms.sh.rej ]; then
     cat build_roms.sh.rej
@@ -34,8 +52,3 @@ fi
 # Compile ROMS
 ./build_roms.sh -j 2 -noclean
 
-# run julia script
-#julia --eval 'using ROMS;  include(joinpath(dirname(pathof(ROMS)),"..","examples","example_config.jl"))'
-
-#cd ~/tmp-test2/LS2v/Simulation1
-#mpirun -np 1 "$BUILD_DIR/romsM" roms.in | tee roms.out
