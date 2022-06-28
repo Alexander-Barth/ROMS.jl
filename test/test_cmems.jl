203a5bdf0cca4f730b532e4aed23c97076fac898
Alexander Barth <barth.alexander@gmail.com>
2022-06-28 22:27:00 +0200

    check if CMEMS_USERNAME is defined

--- test/test_cmems.jl
+++ test/test_cmems.jl
@@ -2,8 +2,6 @@ using ROMS
 using Test
 using Dates
 
-cmems_username = ENV["CMEMS_USERNAME"]
-cmems_password = ENV["CMEMS_PASSWORD"]
 
 # range of longitude
 xr = [7.6, 12.2];
@@ -15,23 +13,35 @@ t0 = DateTime(2019,1,1);
 
 tr = [t0, t0+Dates.Day(1)]
 
-mapping = Dict(
-    # var  product_id
-    :sea_surface_height_above_geoid => ("zos","med-cmcc-ssh-rean-d"),
-    :sea_water_potential_temperature => ("thetao", "med-cmcc-tem-rean-d"),
-    :sea_water_salinity => ("so","med-cmcc-sal-rean-d"),
-    :eastward_sea_water_velocity => ("uo", "med-cmcc-cur-rean-d"),
-    :northward_sea_water_velocity => ("vo", "med-cmcc-cur-rean-d"),
-)
-
 outdir = tempname()
 
-dataset_cmems = ROMS.CMEMS_opendap(cmems_username,cmems_password,mapping,outdir)
+
+datasets = []
+
+if haskey(ENV,"CMEMS_USERNAME")
+    cmems_username = ENV["CMEMS_USERNAME"]
+    cmems_password = ENV["CMEMS_PASSWORD"]
+
+    mapping = Dict(
+        # var  product_id
+        :sea_surface_height_above_geoid => ("zos","med-cmcc-ssh-rean-d"),
+        :sea_water_potential_temperature => ("thetao", "med-cmcc-tem-rean-d"),
+        :sea_water_salinity => ("so","med-cmcc-sal-rean-d"),
+        :eastward_sea_water_velocity => ("uo", "med-cmcc-cur-rean-d"),
+        :northward_sea_water_velocity => ("vo", "med-cmcc-cur-rean-d"),
+    )
+
+
+    dataset_cmems = ROMS.CMEMS_opendap(cmems_username,cmems_password,mapping,outdir)
+    push!(datasets,dataset_cmems)
+end
 
 url = "https://tds.hycom.org/thredds/dodsC/GLBy0.08/expt_93.0"
 dataset_hycom = ROMS.HYCOM(url,outdir);
+push!(datasets,dataset_hycom)
 
-for dataset in [dataset_cmems,dataset_hycom]
+
+for dataset in datasets
     v,(x,y,z,t) = ROMS.load(dataset,:sea_water_potential_temperature,
                         longitude = xr, latitude = yr, time = tr);
 
