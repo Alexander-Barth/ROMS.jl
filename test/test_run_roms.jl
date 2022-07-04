6c8764bf46828539f115d63c00a87a3b0ebbbd4c
Alexander Barth <barth.alexander@gmail.com>
2022-07-04 22:42:54 +0200

    add PATH for macOS

--- test/test_run_roms.jl
+++ test/test_run_roms.jl
@@ -1,7 +1,7 @@
 using ROMS
 using Test
 
-if Sys.islinux() && haskey(ENV,"ROMS_PASSWORD")
+if Sys.isunix() && haskey(ENV,"ROMS_PASSWORD")
     setupscript = joinpath(@__DIR__,"compile_run_ROMS.sh")
     run(`$setupscript`)
     include("example_config.jl")
