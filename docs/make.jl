d2ee4748c0ef1178d5417e4b1e1e30b3bbbe4b4b
Alexander Barth <barth.alexander@gmail.com>
2024-11-19 11:05:50 +0100

    use Literate

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,33 @@
 using Pkg
 Pkg.activate(@__DIR__)
 CI = get(ENV, "CI", nothing) == "true"
-using Documenter, ROMS
+using Documenter
+using ROMS
+import Literate
+
+Literate.markdown(
+    joinpath(@__DIR__, "..", "examples", "plots.jl"),
+    joinpath(@__DIR__, "src"),
+    execute = true,
+    documenter = true,
+    # We add the credit to Literate.jl the footer
+    credit = false,
+)
+
+Literate.notebook(
+    joinpath(@__DIR__, "..", "examples", "plots.jl"),
+    joinpath(@__DIR__, "src"),
+    execute = false,
+)
+
 
 makedocs(
     modules = [ROMS],
     sitename = "ROMS.jl",
     format = Documenter.HTML(
         prettyurls = CI,
+        footer = "Powered by [Documenter.jl](https://github.com/JuliaDocs/Documenter.jl), [Literate.jl](https://github.com/fredrikekre/Literate.jl) and the [Julia Programming Language](https://julialang.org/)"
+
     ),
     pages = [
         "Tutorial" => "index.md",
@@ -16,7 +36,5 @@ makedocs(
     ],
 )
 
-if CI
-    deploydocs(repo = "github.com/Alexander-Barth/ROMS.jl.git",
-               target = "build")
-end
+deploydocs(repo = "github.com/Alexander-Barth/ROMS.jl.git",
+           target = "build")
